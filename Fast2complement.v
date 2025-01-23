afce29c543e86715ac1d49d611032d292d38b90d
jim.wu0708 <yuiop12qwert34@outlook.com>
2025-01-23 13:19:59 -0500

    Added 2's complement module

--- Fast2complement.v
+++ Fast2complement.v
@@ -3,25 +3,26 @@ module Fast2complement (
 	input [63:0]in,
 	output reg [63:0]out
 );
-reg flip='0';
+	reg flip = 1'b0;
 
-integer i;
-initial begin
-	if(enable)begin
-		for(i = 0; i < 64; i = i + 1)begin
-			if (flip=='0') begin
-				out[i] <= in[i];
-				if(in[i]=='1')begin
-					flip='1';
+	integer i;
+
+	always @ (*) begin
+		if (enable) begin
+			for(i = 0; i < 64; i = i + 1)begin
+				if (~flip) begin
+					out[i] = in[i];
+					if(in[i]==1'b1)begin
+						flip=1'b1;
+					end
+				end else begin
+					out[i] = ~in[i];
 				end
-			end else begin
-				out[i] <= ~in[i];
 			end
+		end else begin
+			out = in;
 		end
-	end else begin
-		out <= in;
 	end
-end
 
 
 endmodule
