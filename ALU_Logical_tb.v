774950fcdb373242dd50b377c02b572abcac9f92
Jim Wu <59402743+ytsf0000@users.noreply.github.com>
2025-02-01 23:43:47 -0500

    Initialized ALU logical operation logic

---
+++ ALU_Logical_tb.v
@@ -0,0 +1,11 @@
+module ALU_Logical_tb ();
+
+	reg in_and;
+	reg in_or;
+	reg in_neg;
+	reg in_not;
+	reg [31:0] a;
+	reg [31:0] b;
+	wire [31:0] c;
+
+endmodule
--- MIPS_Proc.v
+++
@@ -1,3 +0,0 @@
-module MIPS_Proc ();
-
-endmodule
