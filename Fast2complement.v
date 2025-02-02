581d2b9d290158c246b432764c7d14ba060416d0
Jim Wu <59402743+ytsf0000@users.noreply.github.com>
2025-02-01 23:44:21 -0500

    Added Fast2scomplement for NEG operation

---
+++ Fast2complement.v
@@ -0,0 +1,29 @@
+module Fast2complement (
+	input enable,
+	input [63:0]in,
+	output reg [63:0]out
+);
+reg flip;
+
+integer i;
+
+always @ (*) begin
+	flip = 1'b0;
+	if (enable) begin
+		for(i = 0; i < 64; i = i + 1)begin
+			if (!flip) begin
+				out[i] <= in[i];
+				if(in[i]==1'b1)begin
+					flip=1'b1;
+				end
+			end else begin
+				out[i] <= ~in[i];
+			end
+		end
+	end else begin
+		out = in;
+	end
+end
+
+
+endmodule
