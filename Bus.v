017d354ce93b80a58661f49e75736d21c4567fa6
jim.wu0708 <yuiop12qwert34@outlook.com>
2025-02-04 18:11:55 -0500

    Edited datapath

--- Bus.v
+++ Bus.v
@@ -1,48 +1,49 @@
 module Bus (
 	//Mux
-	input [31:0]BusMuxInR0, input [31:0]BusMuxInR1, input [31:0]BusMuxInR2, input [31:0]BusMuxInR3, input [31:0]BusMuxInR4, 
-    input [31:0]BusMuxInR5, input [31:0]BusMuxInR6, input [31:0]BusMuxInR7, input [31:0]BusMuxInR8, input [31:0]BusMuxInR9,
-    input [31:0]BusMuxInR10, input [31:0]BusMuxInR11, input [31:0]BusMuxInR12, input [31:0]BusMuxInR13, input [31:0]BusMuxInR14, input [31:0]BusMuxInR15, 
-    input [31:0]BusMuxInHI, input[31:0]BusMuxInLO, input[31:0]BusMuxInZhigh, input [31:0]BusMuxInZlow, input [31:0]BusMuxInPC, input [31:0] BusMuxInMDR, 
-    input [31:0]BusMuxInPort, input [31:0]BusMuxInCin,
-	//Encoder
-	input R0out, R1out, R2out, R3out, R4out, R5out, 
-    input R6out, R7out, R8out, R9out, R10out, 
-    input R11out, R12out, R13out, R14out, R15out,
-    input HIout, LOout, Zhighout, Zlowout, PCout, MDRout,
-    input InPortout, Cout
+	input [31:0] BusMuxInR0, BusMuxInR1, BusMuxInR2, BusMuxInR3, BusMuxInR4, BusMuxInR5, BusMuxInR6, BusMuxInR7,
+					BusMuxInR8, BusMuxInR9, BusMuxInR10, BusMuxInR11, BusMuxInR12, BusMuxInR13, BusMuxInR14, BusMuxInR15,
+					BusMuxInHi, BusMuxInLo, BusMuxInZhi, BusMuxInZlo, BusMuxInPC, BusMuxInMDR, BusMuxInPort, BusMuxInCin,
 
-	
-	output wire [31:0]BusMuxOut
+	//Enoder
+	input R0out, R1out, R2out, R3out, R4out, R5out, 
+			R6out, R7out, R8out, R9out, R10out, R11out, 
+			R12out, R13out, R14out, R15out, HIout, LOout, 
+			Zhighout, Zlowout, PCout, MDRout, InPortout, Cout,
+
+	//Output of Bus Mux:
+	output [31:0] BusMuxOut
 );
 
 reg [31:0]q;
 
 always @ (*) begin
 	if(R0out) q = BusMuxInR0;
-	if(R1out) q = BusMuxInR1;
-	if(R2out) q = BusMuxInR2;
-    if(R3out) q = BusMuxInR3;
-    if(R4out) q = BusMuxInR4;
-    if(R5out) q = BusMuxInR5;
-    if(R6out) q = BusMuxInR6;
-    if(R7out) q = BusMuxInR7;
-    if(R8out) q = BusMuxInR8;
-    if(R9out) q = BusMuxInR9;
-    if(R10out) q = BusMuxInR10;
-    if(R11out) q = BusMuxInR11;
-    if(R12out) q = BusMuxInR12;
-    if(R13out) q = BusMuxInR13;
-    if(R14out) q = BusMuxInR14;
-    if(R15out) q = BusMuxInR15;
-    if(HIout) q = BusMuxInHI;
-    if(LOout) q = BusMuxInLO;
-    if(Zhighout) q = BusMuxInZhigh;
-    if(Zlowout) q = BusMuxInZlow;
-    if(PCout) q = BusMuxInPC;
-    if(MDRout) q = BusMuxInMDR;
-    if(InPortout) q = BusMuxInPort;
-    if(Cout) q = BusMuxInCin;
+	else if(R1out) q = BusMuxInR1;
+	else if(R2out) q = BusMuxInR2;
+	else if(R3out) q = BusMuxInR3;
+	else if(R4out) q = BusMuxInR4;
+	else if(R5out) q = BusMuxInR5;
+	else if(R6out) q = BusMuxInR6;
+	else if(R7out) q = BusMuxInR7;
+	else if(R8out) q = BusMuxInR8;
+	else if(R9out) q = BusMuxInR9;
+	else if(R10out) q = BusMuxInR10;
+	else if(R11out) q = BusMuxInR11;
+	else if(R12out) q = BusMuxInR12;
+	else if(R13out) q = BusMuxInR13;
+	else if(R14out) q = BusMuxInR14;
+	else if(R15out) q = BusMuxInR15;
+	else if(HIout) q = BusMuxInHi;
+	else if(LOout) q = BusMuxInLo;
+	else if(Zhighout) q = BusMuxInZhi;
+	else if(Zlowout) q = BusMuxInZlo;
+	else if(PCout) q = BusMuxInPC;
+	else if(MDRout) q = BusMuxInMDR;
+	else if(InPortout) q = BusMuxInPort;
+	else if(Cout) q = BusMuxInCin;
+	else q = 32'b0;
 end
+
 assign BusMuxOut = q;
+
 endmodule 
--- DataPath.v
+++ DataPath.v
@@ -1,40 +1,45 @@
 module DataPath(
-	input clock, clear,
-	input [7:0] A,
-	input [7:0] RegisterAImmediate,
-	input RZout, RAAout, RBout,
-	input RAin, RBin, RZin
+	input PCout, Zlowout, MDRout, 
+			R3out, R7out, MARin, 
+			Zin, PCin, MDRin, 
+			IRin, Yin, IncPC, 
+			Read, AND, R3in,
+			R4in, R7in, Clock, 
+	input [31:0] Mdatain
 );
 
 wire [7:0] BusMuxOut, BusMuxInRZ, BusMuxInRA, BusMuxInRB;
 
 wire [7:0] Zregin;
 
-//Devices
-register RA(clear, clock, RAin, RegisterAImmediate, BusMuxInRA);
-register RB(clear, clock, RBin, BusMuxOut, BusMuxInRB);
+//Program Counter
+Register PC(clear, clock, BuxMuxOut, BusMuxIn);
 
-register r0(clear, clock, BusMuxOut, BusMuxIn);
-register r1(clear, clock, BusMuxOut, BusMuxIn);
-register r2(clear, clock, BusMuxOut, BusMuxIn);
-register r3(clear, clock, BusMuxOut, BusMuxIn);
-register r4(clear, clock, BusMuxOut, BusMuxIn);
-register r5(clear, clock, BusMuxOut, BusMuxIn);
-register r6(clear, clock, BusMuxOut, BusMuxIn);
-register r7(clear, clock, BusMuxOut, BusMuxIn);
-register r8(clear, clock, BusMuxOut, BusMuxIn);
-register r9(clear, clock, BusMuxOut, BusMuxIn);
-register r10(clear, clock, BusMuxOut, BusMuxIn);
-register r11(clear, clock, BusMuxOut, BusMuxIn);
-register r12(clear, clock, BusMuxOut, BusMuxIn);
-register r13(clear, clock, BusMuxOut, BusMuxIn);
-register r14(clear, clock, BusMuxOut, BusMuxIn);
-register r15(clear, clock, BusMuxOut, BusMuxIn);
+//Devices
+Register RA(clear, clock, RAin, RegisterAImmediate, BusMuxInRA);
+Register RB(clear, clock, RBin, BusMuxOut, BusMuxInRB);
+
+Register R0(clear, clock, BusMuxOut, BusMuxInR0);
+Register R1(clear, clock, BusMuxOut, BusMuxInR1);
+Register R2(clear, clock, BusMuxOut, BusMuxInR2);
+Register R3(clear, clock, BusMuxOut, BusMuxInR3);
+Register R4(clear, clock, BusMuxOut, BusMuxInR4);
+Register R5(clear, clock, BusMuxOut, BusMuxInR5);
+Register R6(clear, clock, BusMuxOut, BusMuxInR6);
+Register R7(clear, clock, BusMuxOut, BusMuxInR7);
+Register R8(clear, clock, BusMuxOut, BusMuxInR);
+Register R9(clear, clock, BusMuxOut, BusMuxIn);
+Register R10(clear, clock, BusMuxOut, BusMuxIn);
+Register R11(clear, clock, BusMuxOut, BusMuxIn);
+Register R12(clear, clock, BusMuxOut, BusMuxIn);
+Register R13(clear, clock, BusMuxOut, BusMuxIn);
+Register R14(clear, clock, BusMuxOut, BusMuxIn);
+Register R15(clear, clock, BusMuxOut, BusMuxIn);
 
 
 //adder
 adder add(A, BusMuxOut, Zregin);
-register RZ(cler, clock, RZin, Zregin, BusMuxInRz);
+Register RZ(cler, clock, RZin, Zregin, BusMuxInRz);
 
 //Bus
 Bus bus(BusMuxInRZ, BusMuxInRA, BusMuxInRB, RZout, RBout, BusMuxOut);
